ebb882d72ad5b1bc1cd9e04fc3ec03c67914cd22
Dark Thank <dktran004@gmail.com>
2024-11-17 15:31:59 +0700

    Add: set up project for PTUD

---
+++ PTUD_QuanLyMayBay/src/GUI/Run.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Run {
+
+    public static void main(String[] args) {
+        GUI_HOME a = new GUI_HOME();
+        a.setVisible(true);
+        a.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        a.setUndecorated(true);
+        a.setVisible(true);
+    }
+}
